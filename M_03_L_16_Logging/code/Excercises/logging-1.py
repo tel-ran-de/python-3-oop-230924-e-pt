8025ef0f0cca81dd8adebf03a76d11fcb70a9099
stureiko <stureiko@gmail.com>
2025-01-09 15:34:55 +0300

    Lesson 16 - Logging

---
+++ M_03_L_16_Logging/code/Excercises/logging-1.py
@@ -0,0 +1,13 @@
+import logging
+
+# Создать и добавить в код логгер по умолчанию.
+# Создать записи о событиях разных уровней от DEBUG до CRITICAL.
+
+
+def main():
+    # your code here
+    pass
+
+
+if __name__ == '__main__':
+    main()
